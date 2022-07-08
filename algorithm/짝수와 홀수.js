a0323d14d0a0a31556ffb0b065af55fb659420b9
juhyunju <2juhyunju@naver.com>
2022-07-08 19:06:43 +0900

    6일차 완료

---
+++ algorithm/짝수와 홀수.js
@@ -0,0 +1,7 @@
+function solution(num) {
+    var answer = '';
+    if(num % 2 === 0){
+        answer = "Even"
+    }else return "Odd"
+    return answer;
+}
---
+++ algorithm/핸드폰 번호 가리기.js
@@ -0,0 +1,5 @@
+function solution(phone_number) {
+    let arr = phone_number.split('');
+    let answer = arr.fill('*',0,-4).join('')
+      return answer;
+  }
