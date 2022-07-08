7e926daee4d11025210810542b22e6246491ae95
juhyunju <2juhyunju@naver.com>
2022-07-08 10:33:43 +0900

    코드 수정

--- algorithm/41.점수에 따른 등급.js
+++ algorithm/41.점수에 따른 등급.js
@@ -1,26 +1,28 @@
 const grade = (score) =>{
-
-    if(score <=100 && score >=90){
-        console.log("A")
-    }else if (score <= 89 && score >= 80){
-        console.log("B")
-    }else if (score <= 79 && score >= 70){
-        console.log("C")
-    }else if (score <= 69 && score >= 60){
-        console.log("D")
-    }else if (score < 59 && score >=0){
-        console.log("F")
-    }else if (score >101 || score < 0){
-        console.log("잘못된 점수입니다.")
+    if(100 < score || 0 > score){
+        return "잘못된 점수입니다."
+    }
+    let answer = '';
+    if(100 >= score && 90 <= score){
+        answer = 'A'
+    }else if (89>=score && 80 <= score){
+        answer = 'B'
+    }else if (79>=score && 70 <= score){
+        answer = 'c'
+    }else if (69>=score && 60 <= score){
+        answer = 'D'
+    }else if(59 >= score){
+        answer = "F"
     }
 
+    return answer
     
 }
 
-grade(105)  // "잘못된 점수입니다"
-grade(-10)  // "잘못된 점수입니다"
-grade(97)   // "A"
-grade(86)   // "B"
-grade(75)   // "C"
-grade(66)   // "D"
-grade(52)   // "F"
+console.log(grade(105))  // "잘못된 점수입니다"
+console.log(grade(-10))  // "잘못된 점수입니다"
+console.log(grade(100))   // "A"
+console.log(grade(86))   // "B"
+console.log(grade(75))   // "C"
+console.log(grade(66))   // "D"
+console.log(grade(52))   // "F"
