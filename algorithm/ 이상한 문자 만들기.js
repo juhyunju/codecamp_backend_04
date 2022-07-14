a94486b262c65cab23473583669815d4b3cc569a
juhyunju <2juhyunju@naver.com>
2022-07-14 15:35:15 +0900

    1차 완료

---
+++ algorithm/ 이상한 문자 만들기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    let answer = '';
+    let idx = 0;
+    for(let i = 0; i<s.length; i++){
+        if(s[i] === ' '){
+            idx = 0;
+            answer += ' '
+        }else {
+            answer += idx % 2 === 0 ? s[i].toUpperCase():s[i].toLowerCase()
+            idx++
+        }
+    }
+    return answer;
+}
---
+++ algorithm/자연수 뒤집어 배열로 만들기.js
@@ -0,0 +1,6 @@
+function solution(n) {
+    let answer = [];
+    answer = n.toString().split('').reverse().map(Number)
+    
+    return answer;
+}
